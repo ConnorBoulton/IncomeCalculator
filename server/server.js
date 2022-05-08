7a441968c67e5d7be5a24ca6bffb88a9eae9a0b7
ConnorBoulton <cjboulton22@gmail.com>
2022-05-07 21:35:56 -0600

    added server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+app.get("/css", function (req, res) {
+  res.sendFile(path.join(__dirname, "../styles.css"));
+});
+
+app.get("/js", function (req, res) {
+  res.sendFile(path.join(__dirname, "../main.js"));
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`port ${port} is running!`));
